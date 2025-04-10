d18eb4b560ca610a1385c37b7121634c9703b8f6
JhoanLT <jhoanlt19@gmail.com>
2025-04-09 23:11:17 -0500

    refactor: remove redundant comment in appointment.routes.js

--- src/appointment/routes/appointment.routes.js
+++ src/appointment/routes/appointment.routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const appointmentController = require('../controllers/appointment.controller');
 
-// Define routes for appointment-related operations
 router.post('/', appointmentController.createAppointment);
 router.get('/', appointmentController.getAllAppointments);
 
